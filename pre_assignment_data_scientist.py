eac91439a4ec8fe2f8696acff4355ac6923a947b
Markus Toivonen <markus.toivonen@hoxhunt.com>
2022-01-10 13:15:21 +0200

    Added pre-assignment files

---
+++ pre_assignment_data_scientist.py
@@ -0,0 +1,21 @@
+# Write your solution here. The function should return an integer.
+def count_users_with_too_many_reused_passwords(data):
+    return
+
+
+if __name__ == "__main__":
+    # Read data first and then pass it to the function
+    # Print out what the funtion returns
+    print(count_users_with_too_many_reused_passwords())
+
+
+# Write your answer here:
+# ___
+# The answer should be a two digit number with both digits being the same (eg. 11)
+
+# Once you are finished, create a pastebin (https://pastebin.com/) from this file.
+# Select the following options:
+# - Syntax highlighting: Python
+# - Paste exposure: Unlisted
+
+# Then reply to the recruiting email with the pastebin link.
